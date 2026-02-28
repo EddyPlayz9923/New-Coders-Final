dc4d38b720547df18592c7a2c0a7cb0ba5934436
EddyPlayz9923 <rkcopp635@gmail.com>
2026-02-28 11:11:03 -0500

    Change color assignments in switch statement

--- SwitchTest.java
+++ SwitchTest.java
@@ -31,7 +31,7 @@ public class SwitchTest {
         this.number = number;
         switch (number) {
             case 1:
-                color = "red";
+                color = "blue";
                 break;
 
             case 2:
@@ -39,7 +39,12 @@ public class SwitchTest {
                 break;
             
             case 3:
-                color = "white";
+                color = "grey";
+                break;
+
+
+            case 5:
+                color = "white"
                 break;
 
             default:
