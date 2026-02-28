2d054ca25e7bec9a3fa8f941004c1948b4f2e45a
EddyPlayz9923 <rkcopp635@gmail.com>
2026-02-28 13:03:02 -0500

    Complete enum and add ice cream flavor logic
    Added Flavor enum for ice cream flavors and updated Check method to include logic for Friday and chocolate flavor.

--- Enums.java
+++ Enums.java
@@ -1,6 +1,6 @@
 package main.java;
 //Finish the given enum and create your own enum for ice cream flavors 
-//check if the day is Friday and that the ice cream flavor if chocolate 
+//check if the day is Friday and that the ice cream flavor is chocolate 
 public class Enums {
     public enum Day {
         Sunday,
@@ -10,7 +10,26 @@ public class Enums {
     }
 
     public void Check(Day day, Flavor flavor) {
-        
-    }
-    public Enums(){}
+        public enum Day
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+      }
+    public enum Flavor {
+        Vanilla,
+        Chocolate,
+        Strawberry,
+        Mint,
 }
+
+    public void Check(Day, day, Flavor flavor) {
+        if (day == Day.Friday && flavor == flavor.Chocolate) {
+            System.out.printIn (It's Friday and the ice cream flavor is chocolate!");
+        }
+
+    
+        
