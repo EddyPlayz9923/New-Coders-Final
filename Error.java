904db429a69e758a241aff7673c8556b51294ad2
EddyPlayz9923 <rkcopp635@gmail.com>
2026-03-07 12:51:37 -0500

    Correct variable types and loop structure in Error.java
    Fix variable initialization and loop syntax errors in Error.java.

--- Error.java
+++ Error.java
@@ -1,12 +1,13 @@
 package main.java;
 
 public class Error {{
-    int j = "Hello";
-    char number = "one";
-    String place = 'notastring'
+    int j = 0;
+    char number = '1';
+    String place = "notastring";
     int string = 1;
-    for (int i == 0, j > 10, i++){
+    for (int i = 0; j < 10; i++) {
         System.out.println "";
+        j++;
     }
     switch (string){
         case 1:
@@ -16,8 +17,9 @@ public class Error {{
             j = 12;
             break;
         case 3:
-            number = 8;
-            break; 
+            number = '8';
+            break; {
+                
 
     }
 }
